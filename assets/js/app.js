b89640676ff5c1dcb07331a5fe8ea41417e4a04b
deconf <kilitary@gmail.com>
2020-05-08 21:12:17 +0300

    work

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,193 +2,259 @@ var myMap;
 var restaurantUrl;
 
 function init() {
-    // Создание карты.
-    myMap = new ymaps.Map('map', {
-        center: [59.939095, 30.315868],
-        zoom: 10
-    });
-    $.get('/?action=maps', function(zones) {
-        zones = JSON.parse(zones);
-        var regionsNumber = 0;
-        $.each(zones, $.proxy(function(i, el) {
-            $.each(el.coords, $.proxy(function(j, region) {
-                var r = region[0];
-                region[0] = region[1];
-                region[1] = r;
+	// Создание карты.
+	myMap = new ymaps.Map('map', {
+		center: [59.939095, 30.315868],
+		zoom: 10
+	});
+	$.get('/?action=maps', function(zones) {
+		zones = JSON.parse(zones);
+		var regionsNumber = 0;
+		$.each(zones, $.proxy(function(i, el) {
+			$.each(el.coords, $.proxy(function(j, region) {
+				var r = region[0];
+				region[0] = region[1];
+				region[1] = r;
 
-                regionsNumber++;
+				regionsNumber++;
 
-                el.coords[j] = region;
-            }, this));
+				el.coords[j] = region;
+			}, this));
 
-            var myPolygon = new ymaps.Polygon(
-                [el.coords],
-                {
-                    hintContent: 'Зона доставки на ' + el.name + ' за ' + el.cost + ' рублей.' + ' Минимальная сумма: ' + el.min_sum
-                },
-                {
-                    fillColor: el.color,
-                    fillOpacity: 0.6,
-                    strokeColor: el.color,
-                    strokeWidth: 0,
-                    strokeOpacity: 1
-                }
-            );
-            myMap.geoObjects.add(myPolygon);
-        }));
-    });
+			var myPolygon = new ymaps.Polygon(
+				[el.coords],
+				{
+					hintContent: 'Зона доставки на ' + el.name + ' за ' + el.cost + ' рублей.' + ' Минимальная сумма: ' + el.min_sum + ' Время доставки: ' + el.delivery_time
+				},
+				{
+					fillColor: el.color,
+					fillOpacity: 0.6,
+					strokeColor: el.color,
+					strokeWidth: 0,
+					strokeOpacity: 1
+				}
+			);
+			myMap.geoObjects.add(myPolygon);
+		}));
+	});
 }
 
-function gotoRestaurant() {
-    $('#dialog-confirm').hide();
-    if(restaurantUrl) {
-        const goal = restaurantUrl.match(/https?:\/\/(.+)$/)[1];
-        ym(62026936, 'reachGoal', goal);
-        gtag('event', goal);
-        document.location.href = restaurantUrl;
-    }
+function gotoRestaurant(obj) {
+	$('#dialog-confirm').hide();
+	let url = $(obj).data('url');
+	const goal = restaurantUrl.match(/https?:\/\/(.+)$/)[1];
+	ym(62026936, 'reachGoal', goal);
+	gtag('event', goal);
+	document.location.href = url;
 }
 
 function searchButton(e) {
-    var e = window.event;
-    e.preventDefault();
-    doSearch('Санкт-петербург, ' + $('#addr').val());
+	var e = window.event;
+	e.preventDefault();
+	doSearch(city + $('#addr').val());
 }
 
 function doSearch(value) {
-    ymaps.geocode(value).then(function(result) {
-        var coords = result.geoObjects.properties._data.metaDataProperty.GeocoderResponseMetaData.Point.coordinates;
-        myMap.geoObjects.add(result.geoObjects.get(0));
-        city = $('.active').data('id');
-        console.log(city);
-        $.get('/?action=calc&city=' + city + '&lat=' + coords[0] + '&lng=' + coords[1], function(data) {
-            data = JSON.parse(data);
+	ymaps.geocode(value).then(function(result) {
+		var coords = result.geoObjects.properties._data.metaDataProperty.GeocoderResponseMetaData.Point.coordinates;
+		myMap.geoObjects.add(result.geoObjects.get(0));
+		city = $('.active').data('id');
+		let htmlCode = '';
+		$.get('/?action=calc&city=' + city + '&lat=' + coords[0] + '&lng=' + coords[1], function(res) {
+			res = JSON.parse(res);
 
-            $('#msg').html(data.msg);
+			console.log(res);
+			if(!res.length) {
+				htmlCode = res.msg;
+			} else {
+				cookie('addr', value);
+			}
 
-            if(data.msg.indexOf('900') !== -1) {
-                restaurantUrl = '';
+			for(var i = 0; i < res.length; i++) {
+				var restaurant = res[i];
+				console.log('rest', restaurant);
 
-                if(typeof data.menu != 'undefined') {
-                    $('#a_div').html('<a href="' + data.menu + '" class="popup__link-btn" id="href-rest" target="_blank">Посмотреть меню</a>');
-                } else {
-                    $('#href-rest').remove();
-                }
-                $('#dialog-confirm').css('display', 'flex');
-            } else {
-                restaurantUrl = data.url;
-                if(typeof $('#href-rest').html() == 'undefined') {
-                    if(typeof data.menu === 'undefined') {
-                        $('#a_div').html('<a href="#" class="popup__link-btn" id="href-rest" onclick="gotoRestaurant()">Посмотреть меню</a>');
-                    } else {
-                        $('#a_div').html('<a href="' + data.menu + '" class="popup__link-btn" id="href-rest" target="_blank">Посмотреть меню</a>');
-                    }
-                }
-                $('#dialog-confirm').css('display', 'flex');
+				let msg = restaurant.msg;
 
-            }
+				if(restaurant.msg.indexOf('900-23-33') !== -1) {
+					restaurantUrl = '';
 
-        });
+					if(typeof restaurant.menu != 'undefined') {
+						htmlCode += '<a href="' + restaurant.menu + '" class="popup-rests__item" id="href-rest" target="_blank">';
+					} else {
+						$('#href-rest').remove();
+					}
 
-    });
+					$('.overlay').fadeIn();
+				} else {
+					restaurantUrl = restaurant.url;
+					//if(typeof restaurant.menu === 'undefined') {
+						htmlCode += '<a href="#" class="popup-rests__item" data-url="' + restaurantUrl + '" onclick="gotoRestaurant(this)">';
+					//} else {
+					//	htmlCode += '<a href="' + restaurant.menu + '" class="popup-rests__item" id="href-rest" target="_blank">';
+					//}
+
+					$('.overlay').fadeIn();
+
+				}
+				htmlCode += '<div class="popup-rests__logo"><img src="/assets/img/logos/' + restaurant.logo + '" alt="logo"></div>' +
+					'              <div class="popup-rests__delivery-text">' + msg + '</div>' +
+					'              <button type="button" class="popup-rests__btn">Посмотреть меню</button>' +
+					'            </a>';
+			}
+			// console.log('html', htmlCode);
+			console.log(res.error);
+
+			if(res.error) {
+				$('.popup-rests__title').html(htmlCode);
+				$('.popup-rests__container').html('');
+			} else {
+				$('.popup-rests__title').html('По Вашему адресу доступна доставка из ресторанов:');
+				$('.popup-rests__container').html(htmlCode);
+			}
+			$('.popup-rests').fadeIn();
+			$('.overlay').fadeIn();
+		});
+
+	});
+}
+
+function accessCookie(cookieName) {
+	var name = cookieName + '=';
+	var allCookieArray = document.cookie.split(';');
+	for(var i = 0; i < allCookieArray.length; i++) {
+		var temp = allCookieArray[i].trim();
+		if(temp.indexOf(name) == 0)
+			return temp.substring(name.length, temp.length);
+	}
+	return '';
+}
+
+function cookie(cookieName, cookieValue) {
+	if(typeof cookieValue === 'undefined') {
+		return accessCookie(cookieName);
+	}
+
+	daysToExpire = 7;
+	var date = new Date();
+	date.setTime(date.getTime() + (daysToExpire * 24 * 60 * 60 * 1000));
+	document.cookie = cookieName + '=' + cookieValue + '; expires=' + date.toGMTString();
 }
 
 $(function() {
-    ymaps.ready(init);
+	ymaps.ready(init);
 
-    $('#popup__close-btn').click(function(e) {
-        $('#dialog-confirm').hide();
-    });
+	//$('#addr').val(cookie('addr'));
 
-    var $addr = $('#addr');
-    var val = $addr.val();
+	$('#popup__close-btn').click(function(e) {
+		$('#dialog-confirm').hide();
+	});
 
-    $addr.autocomplete({
-        appendTo: '.section-main__search',
-        minLength: 2,
-        select: function(event, ui) {
-            doSearch(ui.item.value);
-        },
-        source: function(request, response) {
-            var cty = city === 'spb' ? 'Санкт-Петербург' : 'Москва';
-            val = 'Россия,г ' + cty + ', ' + $addr.val();
-            $.ajax({
-                method: 'POST',
-                url: 'https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/address',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                    'Authorization': 'Token c09bee2729e6b180d922bfdb9e396d66baac6148'
-                },
-                dataType: 'json',
-                data:
-                    JSON.stringify({
-                        query: val,
-                        count: 5
-                    })
-                ,
-                success: function(data) {
-                    response(data.suggestions);
-                }
-            });
-        }
-    });
+	$('.popup-rests__btn-close').click(function() {
+		$('.overlay').fadeOut();
+		$('.popup-rests').fadeOut();
+	});
+
+	var $addr = $('#addr');
+	// var val = $addr.val();
+
+	$addr.autocomplete({
+		appendTo: '.section-main__search',
+		minLength: 2,
+		select: function(event, ui) {
+			doSearch(ui.item.value);
+		},
+		source: function(request, response) {
+			var cty = city === 'spb' ? 'Санкт-Петербург' : 'Москва';
+			val = 'Россия,г ' + cty + ', ' + $addr.val();
+			$.ajax({
+				method: 'POST',
+				url: 'https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/address',
+				headers: {
+					'Accept': 'application/json',
+					'Content-Type': 'application/json',
+					'Authorization': 'Token c09bee2729e6b180d922bfdb9e396d66baac6148'
+				},
+				dataType: 'json',
+				data:
+					JSON.stringify({
+						query: val,
+						count: 5
+					})
+				,
+				success: function(data) {
+					response(data.suggestions);
+				}
+			});
+		}
+	});
+
+	// Metrika и gtag
+	$('.search-button').click(() => {
+		ym(62026936, 'reachGoal', 'check_address');
+		gtag('event', 'check_address');
+	});
+	$('.phone-goal').click(() => {
+		ym(62026936, 'reachGoal', 'header_phone');
+		gtag('event', 'header_phone');
+	});
+
+	//slick slider
+	$('.slider').slick({
+		infinite: true,
+		speed: 500,
+		touchMove: true,
+		arrows: true,
+		centerMode: true,
+		centerPadding: '190px',
+		slidesToShow: 1,
+		prevArrow: $('.slider__prev'),
+		nextArrow: $('.slider__next'),
+		responsive: [
+			{
+				breakpoint: 1550,
+				settings: {
+					centerPadding: '150px'
+				}
+			},
+			{
+				breakpoint: 1450,
+				settings: {
+					centerPadding: '100px'
+				}
+			},
+			{
+				breakpoint: 1320,
+				settings: {
+					centerPadding: '50px'
+				}
+			},
+			{
+				breakpoint: 756,
+				settings: {
+					centerPadding: '0'
+				}
+			}
+		]
+	});
+
+	//switch city
+
+	const phoneNumbers = {
+		spb: '+7 (812) 900-23-33',
+		msk: '+7 (800) 550-79-31'
+	};
+
+	$('.section-main__city-item').on('click', function() {
+		$('.section-main__city-item').removeClass('active');
+		$(this).addClass('active');
+		city = $(this).data('id');
+
+		const phone = phoneNumbers[city];
+		$('.phone-goal').attr('href', phone);
+		$('.phone-goal span').text(phone);
 
 
-    // Metrika и gtag
-    $('.search-button').click(() => {
-        ym(62026936, 'reachGoal', 'check_address');
-        gtag('event', 'check_address');
-    });
-    $('.phone-goal').click(() => {
-        ym(62026936, 'reachGoal', 'header_phone');
-        gtag('event', 'header_phone');
-    });
-
-    //slick slider
-    $('.slider').slick({
-        infinite: true,
-        speed: 500,
-        touchMove: true,
-        arrows: true,
-        centerMode: true,
-        centerPadding: "190px",
-        slidesToShow: 1,
-        prevArrow: $('.slider__prev'),
-        nextArrow: $('.slider__next'),
-        responsive: [
-            {
-                breakpoint: 1550,
-                settings: {
-                    centerPadding: "150px"
-                }
-            },
-            {
-                breakpoint: 1450,
-                settings: {
-                    centerPadding: "100px"
-                }
-            },
-            {
-                breakpoint: 1320,
-                settings: {
-                    centerPadding: "50px"
-                }
-            },
-            {
-                breakpoint: 756,
-                settings: {
-                    centerPadding: "0",
-                }
-            }
-        ]
-    });
-
-    //switch city
-    $('.section-main__city-item').on('click', function() {
-        $('.section-main__city-item').removeClass('active');
-        $(this).addClass('active');
-        city = $(this).data('id');
-    });
+	});
 
 });
--- index.php
+++ index.php
@@ -1,24 +1,28 @@
 <?php
-require('coordinates.php');
+require('restaurants.php');
 require('geo.php');
 require('vendor/autoload.php');
 
-use GeoIp2\Database\Reader;
-
-$reader = new Reader('/usr/share/GeoIP/GeoLite2-City.mmdb');
-$record = $reader->city($_SERVER['REMOTE_ADDR']);
-$city = $record->city->name === 'St Petersburg' ? 'spb' : 'msk';
-
+// TODO: turn on this code before deploy
+//use GeoIp2\Database\Reader;
+//
+//$reader = new Reader('/usr/share/GeoIP/GeoLite2-City.mmdb');
+//$record = $reader->city($_SERVER['REMOTE_ADDR']);
+//$city = $record->city->name === 'St Petersburg' ? 'spb' : 'msk';
+$city = 'spb';
+$rests = [];
 switch(@$_GET['action']) {
     case 'maps':
-        echo json_encode($coordinates);
+        echo json_encode($restaurants);
         break;
     case 'calc':
         $lat = $_GET['lat'];
         $lng = $_GET['lng'];
         $city = $_GET['city'];
-        foreach($coordinates as $c) {
+        $index = 0;
 
+        foreach($restaurants as $c) {
+            $index++;
             if(Geo::pointInPolygon([$lat, $lng], $c['coords'])) {
                 if($city !== $c['city']) {
                     continue;
@@ -26,46 +30,60 @@ switch(@$_GET['action']) {
                 $data = [];
                 $data['cost'] = $c['cost'];
                 $data['url'] = $c['url'];
+                $data['name'] = $c['name'];
+                $data['id'] = $index;
+                if(empty($c['logo'])) {
+                    $data['logo'] = 'def.svg';
+                } else {
+                    $data['logo'] = $c['logo'];
+                }
 
                 if(isset($c['name'])) {
-                    $msg = 'Ваш заказ будет доставлен из ресторана Italy на ' . $c['name'];
+//                    $msg = 'Ваш заказ будет доставлен из ресторана Italy на ' . $c['name'];
+                    $msg = $c['name'] . '<br/>';
                 } else {
                     $msg = '';
                 }
 
                 if($c['cost'] > 0) {
-                    $msg .= '. Доставка ' . $c['cost'] . ' руб';
+                    $msg .= ' Доставка ' . $c['cost'] . ' руб';
                 } elseif($c['cost'] == 0) {
-                    $msg .= '. Доставка бесплатная';
+                    $msg .= ' Доставка бесплатная';
                 }
 
                 if(isset($c['working_hours'])) {
-                    $msg .= '. Время работы ' . $c['working_hours'];
+                    $msg .= ' Время работы ' . $c['working_hours'];
+                }
+
+                if(isset($c['delivery_time'])) {
+                    $msg .= ' Время доставки ' . $c['delivery_time'] . ' мин.';
                 }
 
                 if($c['min_sum'] > 0) {
-                    $msg .= '.<br/> Минимальная сумма заказа ' . $c['min_sum'] . ' руб';
+                    $msg .= '<br/> Минимальная сумма заказа ' . $c['min_sum'] . ' руб';
                 }
 
                 if(isset($c['tel'])) {
-                    $msg .= '. <br/>Заказ можете сделать по телефону <br/><a style="color:red" href="tel:' . $c['tel'] . '">' . $c['tel'] . '</a>';
+                    $msg .= ' <br/>Заказ можете сделать по телефону <br/><div></div><a style="color:red" href="tel:' . $c['tel'] . '">' . $c['tel'] . '</a>';
                 }
 
                 if(isset($c['menu'])) {
                     $data['menu'] = $c['menu'];
                 }
                 $data['msg'] = $msg . '<br/><br/>';
-
-                echo json_encode($data);
-                exit;
+                $rests[] = $data;
             }
         }
 
-        $data = [];
-        $data['msg'] = "Данный адрес не входит в зону Доставки.<br/> Пожалуйста, свяжитесь с нами по телефону " .
-            "<br/><a  style='color:red' href='tel:8-812-900-23-33'>8 (812) 900-23-33</a><br/><br/><br/>";
-        echo json_encode($data);
-        exit;
+        if(!count($rests)) {
+            $data = [];
+            $data['msg'] = "<div style='text-align: center'>Данный адрес не входит в зону Доставки.<br/> Пожалуйста, свяжитесь с нами по телефону " .
+                "<br/><a  style='color:red' href='tel:8-812-900-23-33'>8 (812) 900-23-33</a></div>";
+            $data['error'] = true;
+            echo json_encode($data);
+        } else {
+            echo json_encode($rests);
+        }
         break;
     default:
         include('views/home.php');
